e55885eb3b971bce44ca748826d96d34fd5ff923
Alexander <sacha-ek@yandex.ru>
2026-09-17 18:34:20 +0500

    Config

---
+++ api/configs/config.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+from typing import Any
+
+
+class Config:
+    _isinstance = None
+    _dictionary = {}
+
+    def __new__(cls, *args, **kwargs):
+        if cls._isinstance is None:
+            cls._isinstance = super(Config, cls).__new__(cls)
+
+            config_path = Path(__file__).parents[2] / 'resources' / 'urls.properties'
+            if not config_path.exists():
+                raise FileNotFoundError(f"Config path not found: {config_path}")
+
+            with open(config_path, "r") as f:
+                for line in f:
+                    if "=" in line:
+                        key, value = line.strip().split("=")
+                        cls._dictionary[key] = value
+
+        return cls._isinstance
+
+    @staticmethod
+    def fetch(key: str, default_value: Any = None):
+        return Config()._dictionary.get(key, default_value)
