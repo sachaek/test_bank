01f790f3ae43a81edaaa410629c6b213f0d76c04
Alexander <sacha-ek@yandex.ru>
2026-09-17 21:51:42 +0500

    class Endpoint:

--- api/foundation/endpont.py
+++ api/foundation/endpont.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from typing import Optional, Type
 from api.models.base_model import BaseModel
+from api.models.create_user_request import CreateUserRequest
+from api.models.create_user_response import CreateUserResponse
 
 
 @dataclass
@@ -9,4 +11,10 @@ class EndpointConfiguration:
     request_model: Optional[Type[BaseModel]]
     response_model: Optional[Type[BaseModel]]
 
+
 class Endpoint:
+    ADMIN_CREATE_USER = EndpointConfiguration(
+        request_model=CreateUserRequest,
+        url="/admin/create",
+        response_model=CreateUserResponse
+    )
