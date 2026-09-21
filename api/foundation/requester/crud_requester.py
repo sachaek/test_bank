39f62a58fb3373c3eff84b09f029147a660d0227
Alexander <sacha-ek@yandex.ru>
2026-09-21 23:20:17 +0500

    CrudRequester

--- api/foundation/requester/crud_requester.py
+++ api/foundation/requester/crud_requester.py
@@ -2,6 +2,7 @@ from typing import Optional
 
 import requests
 from requests import Response
+import allure
 
 from api.configs.config import Config
 from api.foundation.http_requester import HttpRequester
@@ -12,18 +13,36 @@ class CrudRequester(HttpRequester):
     def post(self, model: Optional[BaseModel]) -> Response:
         body = model.model_dump() if model is not None else ""
 
+        with allure.step(f"POST request to {self.endpoint.value.url}"):
+            allure.attach(str(body), "Request Body", allure.attachment_type.JSON)
+
         response = requests.post(
             url=f"{Config.fetch("backendUrl")}{self.endpoint.value.url}",
             headers=self.request_spec,
             json=body
         )
+
+        allure.attach(
+            response.text,
+            "Response Body",
+            allure.attachment_type.JSON
+        )
+
         self.response_spec(response)
         return response
 
     def delete(self, user_id: int) -> Response:
+        with allure.step(f"DELETE request to {self.endpoint.value.url}/{user_id}"):
+            allure.attach(f"Deleting user with ID: {user_id}", "Request Info", allure.attachment_type.TEXT)
+
         response = requests.delete(
             url=f"{Config.fetch("backendUrl")}{self.endpoint.value.url}/{user_id}",
             headers=self.request_spec
+        )
+        allure.attach(
+            response.text,
+            "Response Body",
+            allure.attachment_type.JSON
         )
         self.response_spec(response)
         return response
