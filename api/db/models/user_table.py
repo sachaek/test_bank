307d3b231c358f66502d996e1817c9a183b20c39
Alexander <sacha-ek@yandex.ru>
2026-09-23 23:22:45 +0500

    change name user users in tables

--- api/db/models/user_table.py
+++ api/db/models/user_table.py
@@ -2,13 +2,13 @@ from sqlalchemy import Column, Integer, String, DateTime
 from api.db.base import Base
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "user"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     username = Column(String, unique=True, nullable=False)
     password = Column(String, nullable=False)
     role = Column(String, nullable=False)
-    deleted_at = Column(DateTime, nullable=False)
+    deleted_at = Column(DateTime, nullable=True)
 
     def __repr__(self):
         return f"<User(id={self.id}, username='{self.username}', role='{self.role}', deleted_at='{self.deleted_at}')>"
