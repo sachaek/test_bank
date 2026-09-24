b859c6a498573f8e52b8beb5ef0b77888eb3d152
Alexander <sacha-ek@yandex.ru>
2026-09-24 22:19:01 +0500

    create_user

--- api/db/crud/user_crud.py
+++ api/db/crud/user_crud.py
@@ -7,3 +7,11 @@ class UserCrudDB:
     @staticmethod
     def get_user_by_username(db: Session, username: str) -> User | None:
         return db.query(User).filter_by(username=username).first()
+
+    @staticmethod
+    def create_user(db: Session, username: str, password: str, role: str) -> User:
+        user = User(username=username, password=password, role=role)
+        db.add(user)
+        db.commit()
+        db.refresh(user)
+        return user
