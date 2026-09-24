af4d678fdfabe19e6f253a25900eb65283a6e10b
Alexander <sacha-ek@yandex.ru>
2026-09-24 22:02:47 +0500

    filter_by и filter fix

--- api/db/crud/account_crud.py
+++ api/db/crud/account_crud.py
@@ -6,4 +6,4 @@ from api.db.models.account_table import Account
 class AccountCrudDB:
     @staticmethod
     def get_account_by_id(db: Session, account_id: int) -> Account | None:
-        return db.query(Account).filter(id=account_id).first()
+        return db.query(Account).filter_by(id=account_id).first()
--- tests/create_account_test.py
+++ tests/create_account_test.py
@@ -1,14 +1,17 @@
 import pytest
+from sqlalchemy.orm import Session
 
+from api.classes.api_manager import ApiManager
+from api.db.crud.account_crud import AccountCrudDB as Account
 from api.models.create_user_request import CreateUserRequest
-from api.requests.create_user_requester import CreateUserRequester
-from specs.request_specs import RequestSpecs
-from specs.response_specs import ResponseSpecs
-from api.requests.create_account_requester import CreateAccountRequester
 
 
 @pytest.mark.api
 class TestCreateAccount:
-    def test_create_account(self, api_manager, create_user_request):
+    def test_create_account(self, db_session: Session, api_manager: ApiManager, create_user_request: CreateUserRequest):
         response = api_manager.user_steps.create_account(create_user_request)
         assert response.balance == 0
+
+        account_from_db = Account.get_account_by_id(db=db_session, account_id=response.id)
+        assert account_from_db.id == response.id, f"Аккаунт с id '{response.id}' не был создан в базе данных."
+        assert account_from_db.balance is not None, f"Аккаунт с id '{response.id}' был создан, но баланс не был установлен."
